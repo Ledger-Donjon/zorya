d4a47d1ed6b55eac36ca9bff453e798240f57227
kgorna <karolinagorna@hotmail.fr>
2024-08-20 10:42:31 +0200

    add handle_float_equal

--- src/concolic/executor.rs
+++ src/concolic/executor.rs
@@ -112,7 +112,7 @@ impl<'ctx> ConcolicExecutor<'ctx> {
             Opcode::FloatAbs => panic!("Opcode is not implemented yet"), //executor_float::handle_float_abs(self, instruction),
             Opcode::FloatAdd => panic!("Opcode is not implemented yet"), //executor_float::handle_float_add(self, instruction),
             Opcode::FloatDiv => panic!("Opcode is not implemented yet"), //executor_float::handle_float_div(self, instruction),
-            Opcode::FloatEqual => panic!("Opcode is not implemented yet"), //executor_float::handle_float_equal(self, instruction),
+            Opcode::FloatEqual => executor_float::handle_float_equal(self, instruction),
             Opcode::FloatLess => panic!("Opcode is not implemented yet"), //executor_float::handle_float_less(self, instruction),
             Opcode::FloatLessEqual => panic!("Opcode is not implemented yet"), //executor_float::handle_float_lessequal(self, instruction),
             Opcode::FloatMult => panic!("Opcode is not implemented yet"), //executor_float::handle_float_mult(self, instruction),
