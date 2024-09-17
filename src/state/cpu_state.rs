4c6afd29f8f9f405d6c568f54e40131900e8984f
kgorna <karolinagorna@hotmail.fr>
2024-09-17 14:41:42 +0200

    debigging

--- src/state/cpu_state.rs
+++ src/state/cpu_state.rs
@@ -520,9 +520,16 @@ impl<'ctx> CpuState<'ctx> {
                     println!("full_reg_size: {}", full_reg_size);
                     println!("new_size: {}", new_size);
                     println!("bit_offset: {}", bit_offset);
+                    println!("new_value.symbolic: {:?}", new_value.symbolic);
+                    println!("new_value.symbolic.to_bv: {:?}", new_value.symbolic.to_bv(self.ctx));
+                    println!("new_value.symbolic.to_bv.zero_extend: {:?}", new_value.symbolic.to_bv(self.ctx).zero_ext(full_reg_size as u32 - new_size));
+                    println!("new_value.symbolic.to_bv.zero_extend.bvshl: {:?}", new_value.symbolic.to_bv(self.ctx).zero_ext(full_reg_size as u32 - new_size).bvshl(&BV::from_u64(self.ctx, bit_offset, full_reg_size as u32)));
 
                     // Ensure small symbolic values remain as Int
-                    let new_symbolic_value = new_value.symbolic.to_bv(self.ctx).zero_ext(full_reg_size as u32 - new_size).bvshl(&BV::from_u64(self.ctx, bit_offset, full_reg_size as u32));
+                    let new_symbolic_value = new_value.symbolic
+                        .to_bv(self.ctx)
+                        .zero_ext(full_reg_size as u32 - new_size)
+                        .bvshl(&BV::from_u64(self.ctx, bit_offset, full_reg_size as u32));
 
                     if new_symbolic_value.get_z3_ast().is_null() {
                         println!("Error: New symbolic value is null");
